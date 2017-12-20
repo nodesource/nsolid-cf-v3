9d370bd3ba08c233adacd460286657c7d4b71989
Patrick Mueller <pmuellr@nodesource.com>
2017-12-20 17:36:41 -0500

    updated to use console 3.0.3

--- nsolid-console-app/setup.sh
+++ nsolid-console-app/setup.sh
@@ -4,10 +4,10 @@
 PRODUCT=console
 
 # bundle version
-BUNDLE=3.0.2
+BUNDLE=3.0.3
 
 # version of the product
-VERSION=3.0.2
+VERSION=3.0.3
 
 # name of the tarball that will be uploaded to Cloud Foundry via cf push
 TARBALL=nsolid-$PRODUCT-linux-x64.tar.gz
--- tools/push-samples-pjm.sh
+++ tools/push-samples-pjm.sh
@@ -4,21 +4,21 @@
 
 cd nsolid-console-app
 ./setup.sh
-cf push -f manifest-pjm.yml
+cf push -f manifest-pjm.yml &
 cd ..
 
 cd sample-apps/native
-cf push -f manifest-pjm.yml
+cf push -f manifest-pjm.yml &
 cd ../..
 
 cd sample-apps/print-versions-4x
-cf push -f manifest-pjm.yml
+cf push -f manifest-pjm.yml &
 cd ../..
 
 cd sample-apps/print-versions-6x
-cf push -f manifest-pjm.yml
+cf push -f manifest-pjm.yml &
 cd ../..
 
 cd sample-apps/print-versions-8x
-cf push -f manifest-pjm.yml
+cf push -f manifest-pjm.yml &
 cd ../..
