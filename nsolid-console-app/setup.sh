c9c0f3d77278f478a00bec5bf275249f0cde88ef
Patrick Mueller <pmuellr@nodesource.com>
2018-12-04 08:50:09 -0500

    updated to use console 3.4.3

--- nsolid-console-app/setup.sh
+++ nsolid-console-app/setup.sh
@@ -4,10 +4,10 @@
 PRODUCT=console
 
 # bundle version
-BUNDLE=3.4.2
+BUNDLE=3.4.3
 
 # version of the product
-VERSION=3.4.2
+VERSION=3.4.3
 
 # name of the tarball that will be uploaded to Cloud Foundry via cf push
 TARBALL=nsolid-$PRODUCT-linux-x64.tar.gz
